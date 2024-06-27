8d649bbecdcd2573cf92569fb6d29d0e4f0f1305
Kator2509 <Kator2509@mail.ru>
2024-06-27 20:05:19 +0400

    Fix chat. Add exception try to send empty message. Add MessageIsEmpty in ChatConfig.yml.
    Test's success.
    Need write a command for admin to create a custom chat.
    Need test with permission.
    Delete default permission on OP.

--- src/main/java/org/pol/Chat/ChatEvent.java
+++ src/main/java/org/pol/Chat/ChatEvent.java
@@ -70,157 +70,77 @@ public class ChatEvent extends ChatConfig implements Listener, CommandExecutor
             suffix = "";
         }
 
-        ConfigurationSection sec = getConfigChat().getConfigurationSection("Chats");
-        try {
-            for (String key : sec.getKeys(false)) {
-                if (Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Symbol")).charAt(0) == message.charAt(0)) {
-                    if (event.getPlayer().hasPermission(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Permission")))) {
-                        for (Player player : Bukkit.getOnlinePlayers()) {
-                            if (player.hasPermission(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Permission")))) {
-                                player.sendMessage(Objects.requireNonNull(translateColor(
-                                        Objects.requireNonNull(Objects.requireNonNull(getConfigChat().getString("Chats." + key + "Format"))
-                                                .replace("%PREFIX", prefix)
-                                                .replace("%PLAYER", playerName)
-                                                .replace("%SUFFIX", suffix)
-                                                .replace("%MESSAGE", message)))));
+        if(message.length() != 1) {
+            ConfigurationSection sec = getConfigChat().getConfigurationSection("Chats");
+            try {
+                for (String key : sec.getKeys(false)) {
+                    if (Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Symbol")).charAt(0) == message.charAt(0)) {
+                        if (event.getPlayer().hasPermission(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Permission")))) {
+                            for (Player player : Bukkit.getOnlinePlayers()) {
+                                if (player.hasPermission(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Permission")))) {
+                                    player.sendMessage(Objects.requireNonNull(translateColor(
+                                            Objects.requireNonNull(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Format"))
+                                                    .replace("%PREFIX", prefix)
+                                                    .replace("%PLAYER", playerName)
+                                                    .replace("%SUFFIX", suffix)
+                                                    .replace("%MESSAGE", message.substring(1))))));
+                                }
                             }
+                            Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(translateColor(
+                                    Objects.requireNonNull(Objects.requireNonNull(getConfigChat().getString("Chats." + key + ".Format"))
+                                            .replace("%PREFIX", prefix)
+                                            .replace("%PLAYER", playerName)
+                                            .replace("%SUFFIX", suffix)
+                                            .replace("%MESSAGE", message.substring(1))))));
+                        } else {
+                            event.getPlayer().sendMessage(Objects.requireNonNull(translateColor(getConfigChat().getString("DontHavePermission"))));
                         }
-                        Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(translateColor(
-                                Objects.requireNonNull(Objects.requireNonNull(getConfigChat().getString("Chats." + key + "Format"))
-                                        .replace("%PREFIX", prefix)
-                                        .replace("%PLAYER", playerName)
-                                        .replace("%SUFFIX", suffix)
-                                        .replace("%MESSAGE", message)))));
-                    } else {
-                        event.getPlayer().sendMessage(Objects.requireNonNull(translateColor(getConfigChat().getString("DontHavePermission"))));
+                        event.setCancelled(true);
                     }
+                }
+                if (getConfigChat().getBoolean("localChatEnable") && !event.isCancelled()) {
+                    Player playerSender = event.getPlayer();
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.getLocation().distance(playerSender.getLocation()) <= getConfigChat().getInt("rangeChat")) {
+                            player.sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
+                                                    Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
+                                                            .replace("%PREFIX", prefix))
+                                            .replace("%PLAYER", event.getPlayer().getName())
+                                            .replace("%SUFFIX", suffix))
+                                    .replace("%MESSAGE", message));
+                        }
+                    }
+                    Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
+                                            Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
+                                                    .replace("%PREFIX", prefix))
+                                    .replace("%PLAYER", playerName)
+                                    .replace("%SUFFIX", suffix))
+                            .replace("%MESSAGE", message));
                     event.setCancelled(true);
+                } else if (!event.isCancelled()) {
+                    event.setFormat(translateColor(Objects.requireNonNull(getConfigChat().getString("MessageFormat"))
+                            .replace("%PREFIX", prefix)
+                            .replace("%PLAYER", playerName)
+                            .replace("%SUFFIX", suffix)
+                            .replace("%MESSAGE", message)));
                 }
-            }
-            if(getConfigChat().getBoolean("LocalChatEvent"))
-            {
-                Player playerSender = event.getPlayer();
-                for (Player player:Bukkit.getOnlinePlayers()) {
-                    if (player.getLocation().distance(playerSender.getLocation()) <= getConfigChat().getInt("rangeChat")) {
-                        player.sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                                Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                                        .replace("%PREFIX", prefix))
-                                                        .replace("%PLAYER", event.getPlayer().getName())
-                                                        .replace("%SUFFIX", suffix))
-                                                        .replace("%MESSAGE", message));
-                    }
-                }
-                Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                        Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                                .replace("%PREFIX", prefix))
-                                                .replace("%PLAYER", playerName)
-                                                .replace("%SUFFIX", suffix))
-                                                .replace("%MESSAGE", message));
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                event.getPlayer().sendMessage(translateColor("&c[PoL] - Error with config."));
                 event.setCancelled(true);
             }
-            else {
-                event.setFormat(translateColor(Objects.requireNonNull(getConfigChat().getString("MessageFormat"))
+        }
+        else
+        {
+            if(!Objects.requireNonNull(getConfigChat().getString("MessageIsEmpty")).isEmpty()) {
+                event.getPlayer().sendMessage(translateColor(Objects.requireNonNull(getConfigChat().getString("MessageIsEmpty"))
                         .replace("%PREFIX", prefix)
                         .replace("%PLAYER", playerName)
                         .replace("%SUFFIX", suffix)
                         .replace("%MESSAGE", message)));
             }
-        } catch (NullPointerException e)
-        {
-            e.printStackTrace();
-            event.getPlayer().sendMessage("&c[PoL] - Error with config.");
             event.setCancelled(true);
         }
-
-
-
-
-        /*
-        if(getConfigChat().getBoolean("LocalChatEnable") && event.getPlayer().hasPermission(permOnLocalChat))
-        {
-            Player playerSender = event.getPlayer();
-            for (Player player:Bukkit.getOnlinePlayers()) {
-                if (player.getLocation().distance(playerSender.getLocation()) <= getConfigChat().getInt("rangeChat")) {
-                    player.sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                            Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                                    .replace("%PREFIX", prefix))
-                                    .replace("%PLAYER", event.getPlayer().getName())
-                                    .replace("%SUFFIX", suffix))
-                            .replace("%MESSAGE", event.getMessage()));
-                }
-            }
-            Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                    Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                            .replace("%PREFIX", prefix))
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix))
-                    .replace("%MESSAGE", event.getMessage()));
-            event.setCancelled(true);
-        }
-
-
-
-        if(!getConfigChat().getBoolean("GlobalChatEnable") && event.getPlayer().hasPermission(permOnChat))
-        {
-            event.setFormat(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                    Objects.requireNonNull(getConfigChat().getString("MessageFormat"))
-                            .replace("%PREFIX", prefix)
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix)
-                            .replace("%MESSAGE", event.getMessage()))));
-        }
-        else if(getConfigChat().getBoolean("GlobalChatEnable") && event.getPlayer().hasPermission(permOnGlobalChat)
-                && event.getMessage().charAt(0) == Objects.requireNonNull(getConfigChat().getString("SymbolGlobalChat")).charAt(0))
-        {
-            event.setFormat(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                    Objects.requireNonNull(getConfigChat().getString("GlobalChatFormat"))
-                            .replace("%PREFIX", prefix)
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix)
-                            .replace("%MESSAGE", event.getMessage().substring(1)))));
-        }
-        else if(getConfigChat().getBoolean("TradeChatEnable") && event.getPlayer().hasPermission(permOnTradeChat)
-                && event.getMessage().charAt(0) == Objects.requireNonNull(getConfigChat().getString("SymbolTradeChat")).charAt(0))
-        {
-            event.setFormat(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                    Objects.requireNonNull(getConfigChat().getString("TradeChatFormat"))
-                            .replace("%PREFIX", prefix)
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix)
-                            .replace("%MESSAGE", event.getMessage().substring(1)))));
-        }
-        else if(getConfigChat().getBoolean("GlobalChatEnable") && event.getPlayer().hasPermission(permOnLocalChat))
-        {
-            Player playerSender = event.getPlayer();
-            for (Player player:Bukkit.getOnlinePlayers()) {
-                if (player.getLocation().distance(playerSender.getLocation()) <= getConfigChat().getInt("rangeChat")) {
-                    player.sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                            Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                                    .replace("%PREFIX", prefix))
-                                    .replace("%PLAYER", event.getPlayer().getName())
-                                    .replace("%SUFFIX", suffix))
-                            .replace("%MESSAGE", event.getMessage()));
-                }
-            }
-            Bukkit.getConsoleSender().sendMessage(Objects.requireNonNull(ChatColor.translateAlternateColorCodes('&',
-                                    Objects.requireNonNull(getConfigChat().getString("LocalChatFormat"))
-                                            .replace("%PREFIX", prefix))
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix))
-                    .replace("%MESSAGE", event.getMessage()));
-            event.setCancelled(true);
-        }
-        else
-        {
-            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    Objects.requireNonNull(getConfigChat().getString("DontHavePermission"))
-                            .replace("%PREFIX", prefix)
-                            .replace("%PLAYER", event.getPlayer().getName())
-                            .replace("%SUFFIX", suffix)
-                            .replace("%MESSAGE", event.getMessage())));
-            event.setCancelled(true);
-        }
-        */
     }
 
     public static String translateColor(String string)
