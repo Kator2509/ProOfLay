1c3b08961c7876db45fecb4af12539a5761b7add
Kator2509 <alfach.2019@mail.ru>
2025-02-21 13:30:35 +0300

    Change pom.xml. Need tests on local machine.

--- src/main/java/org/graphic/test/modul/ProFlayCommandLoader.java
+++ src/main/java/org/graphic/test/modul/ProFlayCommandLoader.java
@@ -1,8 +1,5 @@
 package org.graphic.test.modul;
 
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 public abstract class ProFlayCommandLoader
 {
     protected String message;
@@ -10,16 +7,6 @@ public abstract class ProFlayCommandLoader
     public ProFlayCommandLoader(){}
 
     public void commandRun()
-    {
-
-    }
-
-    protected void run(Player player, String[] args)
-    {
-
-    }
-
-    protected void run(CommandSender sender, String[] args)
     {
 
     }
