490cb42e1cf518f9b83adb1f8787ccb38c2b07ae
ponfertato <ponfertato@ya.ru>
2025-03-14 13:20:53 +0300

    ⚡️Global optimizations of the project
    - Оптимизация и упрощение кода
    - Улучшение потокобезопасности
    - Исправление работы с памятью и ресурсами
    - Удаление избыточных проверок
    - Автоматическая регистрация компонентов
    - Улучшенное форматирование сообщений

--- src/main/java/org/graphic/test/Commander/modul/ProFlayCommandListener.java
+++ src/main/java/org/graphic/test/Commander/modul/ProFlayCommandListener.java
@@ -7,52 +7,36 @@ import org.graphic.test.Commander.ProFlayCommand;
 import org.graphic.test.Commander.TestCommand;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class ProFlayCommandListener
-{
-    protected static Map<String, ProFlayCommand> commandMap = new HashMap<String, ProFlayCommand>();
+public class ProFlayCommandListener {
+    protected static final Map<String, ProFlayCommand> commandMap = new ConcurrentHashMap<>();
     private static boolean moduleIsEnable = false;
 
-    public ProFlayCommandListener() {}
-
-    /*Конструктор листенера команд.*/
-    public ProFlayCommandListener(@NotNull Plugin plugin)
-    {
+    // Конструктор с плагином
+    public ProFlayCommandListener(@NotNull Plugin plugin) {
         this.registerDefaultCommands();
         moduleIsEnable = true;
     }
 
-    /*Метод регистрации дефолтных команд системы.
-    * Можно перезаписать их и внести собственную логику, если вызвать register(ProFlayCommand command, String name).*/
-    public void registerDefaultCommands()
-    {
-        if
-        (!(
-                this.register(new TestCommand(), false)
-        ))
-        {
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] Default command not load.");
+    // Дефолтный конструктор
+    public ProFlayCommandListener() {}
+
+    public void registerDefaultCommands() {
+        if (!this.register(new TestCommand(), false)) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] Default command not loaded.");
         }
     }
 
-    /*Метод регистрации внутри системы и проверка на запущенный модуль.
-    * Если модуль запущен moduleIsEnable = true, то остановка регистрации и лог сообщение.
-    * Если команда зарегистрирована isRegistered = true, то остановка регистрации и лог сообщение.
-    * Если ни одно условие не выполняется, то команда регистрируется в системе для последующей его вызова.*/
-    public boolean register(@NotNull ProFlayCommand command, boolean override)
-    {
-        if (moduleIsEnable)
-        {
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] You trying to register command after loading server - " + command.getLabel());
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] If you load your plugin after. Change after on before in your plugin.yml.");
+    public boolean register(@NotNull ProFlayCommand command, boolean override) {
+        if (moduleIsEnable) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] Late command registration: " + command.getLabel());
             return false;
         }
 
-        if(isRegistered(command))
-        {
-            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] This " + command.getName() + " already registered in system.");
+        if (commandMap.containsKey(command.getLabel())) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "[ProFlay] Command already registered: " + command.getName());
             return false;
         }
 
@@ -60,15 +44,11 @@ public class ProFlayCommandListener
         return true;
     }
 
-    /*Производит проверка на наличие команды.*/
-    public boolean isRegistered(@NotNull ProFlayCommand command)
-    {
+    public boolean isRegistered(@NotNull ProFlayCommand command) {
         return commandMap.containsValue(command);
     }
 
-    /*Получить определенную команду.*/
-    public ProFlayCommand getProFlayCommand(@NotNull String name)
-    {
+    public ProFlayCommand getProFlayCommand(@NotNull String name) {
         return commandMap.get(name);
     }
 }
--- src/main/java/org/graphic/test/Commander/modul/ProFlayCommandTransfer.java
+++ src/main/java/org/graphic/test/Commander/modul/ProFlayCommandTransfer.java
@@ -14,35 +14,32 @@ import org.jetbrains.annotations.NotNull;
 public class ProFlayCommandTransfer extends ProFlayCommandListener implements CommandExecutor {
     protected Configuration config = ConfigurationLoader.getConfiguration("main");
 
-    /*Класс представляет собой трансфер, который предоставляет ProFlayCommand
-    * выполняться в системе с собственными методами.*/
+    public ProFlayCommandTransfer() {
+        super();
+    }
 
-    public ProFlayCommandTransfer(){}
-
-    public ProFlayCommandTransfer(@NotNull Plugin plugin)
-    {
-        super(plugin);
+    public ProFlayCommandTransfer(@NotNull Plugin plugin) {
+        super(plugin); // Теперь вызывает правильный конструктор
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args)
-    {
-        if(commandMap.containsKey(label) && sender instanceof Player)
-        {
-            return commandMap.get(label).run(sender, args);
-        }
-        else if(sender instanceof ConsoleCommandSender)
-        {
-            return commandMap.get(label).consoleRun(args);
+    public boolean onCommand(@NotNull CommandSender sender,
+                             @NotNull Command command,
+                             @NotNull String label,
+                             @NotNull String[] args) {
+        if (commandMap.containsKey(label)) {
+            if (sender instanceof Player) {
+                return commandMap.get(label).run(sender, args);
+            } else if (sender instanceof ConsoleCommandSender) {
+                return commandMap.get(label).consoleRun(args);
+            }
         }
 
-
-        if(!commandMap.containsKey(label))
-        {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("OutCommandMessage")));
+        if (!commandMap.containsKey(label)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    config.getString("OutCommandMessage")));
             return true;
         }
-
 
         return false;
     }
